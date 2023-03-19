3f01193ca801d6e6d488738cba63d6980c334e2a
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-19 09:14:21 +0800

    update post task to backend

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import Task from "./Task";
 import TaskForm from "./TaskForm";
+import axios from "axios";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const TaskList = () => {
   const [formData, setFormData] = useState({
@@ -16,13 +20,32 @@ const TaskList = () => {
     setFormData({ ...formData, name: e.target.value });
   };
 
-  const createTaskForm = (e) => {
+  const createTaskForm = async (e) => {
     e.preventDefault();
     console.log(formData);
+    if (name === "") {
+      return toast.error("Input field cannot be empty");
+    }
+    try {
+        // const res = await fetch("http://127.0.0.1:5000/api/tasks", {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-type": "application/json",
+        //   },
+        //   body: JSON.stringify(formData),
+        // });
+        // const data = await res.json();
+        // console.log(data);
+      await axios.post("http://127.0.0.1:5000/api/tasks", formData);
+      setFormData({ ...formData, name: "" });
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
     <div>
+      <ToastContainer />
       <h2>Task Manager</h2>
       <TaskForm
         name={name}
